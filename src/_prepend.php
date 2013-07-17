1da89e7965d3de766987e27aae14e56acbdc9960
akewea <akewea@gmail.com>
2013-07-17 10:53:24 +0200

    Préparation de la nouvelle version

--- src/_prepend.php
+++ src/_prepend.php
@@ -7,6 +7,9 @@ if (!defined('DC_RC_PATH')) { return; }
 $GLOBALS['__autoload']['fostrak'] = dirname(__FILE__).'/inc/class.fostrak.php';
 $GLOBALS['__autoload']['fostrakExtMedia'] = dirname(__FILE__).'/inc/lib.fostrak.extensions.php';
 
+# Nouveau type de post
+#$core->setPostType('fostrak', 'plugin.php?p=fostrak&projectid=%s', $core->blog->settings->foostrak->fostrak_base_url.'/(.*)$');
+
 //# Nouveau type de post
 //$core->setPostType('litraak', 'plugin.php?p=litraak&projectid=%s', $core->blog->settings->litraak->litraak_basename_url.'/%s/');
 //
